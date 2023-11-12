764bbb321e86bd8657ba3c96262aaa56427d3688
vinay M <vinaym@vinays-MacBook-Air.local>
2023-11-12 19:48:22 +0530

    initial module set-up

---
+++ src/app/dashboard/call-history/call-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-call-history',
+  templateUrl: './call-history.component.html',
+  styleUrls: ['./call-history.component.scss']
+})
+export class CallHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/dashboard/web-rtc-connect/web-rtc-connect.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-web-rtc-connect',
+  templateUrl: './web-rtc-connect.component.html',
+  styleUrls: ['./web-rtc-connect.component.scss']
+})
+export class WebRtcConnectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
